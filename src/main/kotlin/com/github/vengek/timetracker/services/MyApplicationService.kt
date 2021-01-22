c68a7c878e50dbe4c3189b7be833b64a1a21dcee
GitHub Action <action@github.com>
2021-01-22 06:31:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/vengek/timetracker/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/vengek/timetracker/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.vengek.timetracker.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.vengek.timetracker.MyBundle
 
 class MyApplicationService {
 
